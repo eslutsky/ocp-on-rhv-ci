81e32fa493284d55b9868ab8c071f780d4421772
Evgeny Slutsky <eslutsky@redhat.com>
2019-11-14 13:55:45 +0200

    assigning  static access ips to gce vms

---
+++ assign-access-ips.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source env.sh
+
+free_ip=""
+
+vms=()
+## assigned public  static ip to engine (if missing)
+get_vms_without_public_ips  'ocp-rhv-vm-engine'
+if [ $? -eq 0 ] ;
+then
+  get_free_public_ip '.*static-ip-engine'
+  if [ $? -eq 0 ] ;
+  then
+    echo "assigning public ip - ${free_ip} to ${vms} VM"
+    gcloud compute instances add-access-config ${vms} --address ${free_ip} --zone us-central1-b
+  fi
+fi
+
+## assigned public  static ip to first host (if missing)
+get_vms_without_public_ips  '^ocp-rhv-nested-vm-host$'
+if [ $? -eq 0 ] ;
+then
+  get_free_public_ip 'ocp-rhv-static-ip-host-00'
+  if [ $? -eq 0 ] ;
+  then
+    echo "assigning public ip - ${free_ip} to ${vms} VM"
+    gcloud compute instances add-access-config ${vms} --address ${free_ip} --zone us-central1-b
+  fi
+fi
+
+## assigned public ips to the scaled hosts
+get_vms_without_public_ips '^ocp-rhv-nested-vm-host-.*'
+if [ $? -eq 0 ] ;
+then
+  for element in "${vms[@]}"
+  do
+      get_free_public_ip 'ocp-rhv-static-ip-host-[0-9][1-9]'
+      if [ $? -eq 0 ] ;
+      then
+          echo "assigning public ip - ${free_ip} to $element VM"
+          gcloud compute instances add-access-config $element --address ${free_ip} --zone us-central1-b
+      fi
+  done
+fi
--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
   function terraform() {
       [[ -z "$rhv_host_count" ]] && rhv_host_count=1
 
@@ -39,4 +40,38 @@
     -e MY_UID=$(id -u) \
     -e MY_GID=$(id -g) \
     -v ${HOME}/.ssh/:/home/ansible/.ssh/:ro  -v $(pwd):/data quay.io/eslutsky/ansible:latest-tools ansible-doc "$@";
+    }
+
+    function gcloud(){
+      docker run --rm \
+      -e CLOUDSDK_CORE_PROJECT=openshift-gce-devel \
+      -e CLOUDSDK_CONFIG=/config/mygcloud \
+      -v $(pwd)/mygcloud:/config/mygcloud \
+      -v $(pwd):/certs \
+      gcr.io/google.com/cloudsdktool/cloud-sdk:alpine gcloud "$@";
+
+    }
+
+    function get_free_public_ip()
+    {
+      local regex_filter=$1
+      free_ip=""
+      free_ip=$(gcloud compute addresses list --filter="name~'${regex_filter}' \
+      AND status:RESERVED" \
+      --format='value(ADDRESS)' | head -1) >/dev/null
+      [[ -z "$free_ip" ]] && return 1
+      return 0
+    }
+
+    function get_vms_without_public_ips()
+    {
+      #[ "$DEBUG" == 'true' ] && set -x
+      local regex_filter=$1
+      vms=()
+      vms=(`gcloud compute instances list --filter="name~'${regex_filter}' \
+      AND -EXTERNAL_IP:*" --format='value(NAME)'`)
+      [[ -z "$vms" ]] && return 1
+
+      return 0
+
     }
