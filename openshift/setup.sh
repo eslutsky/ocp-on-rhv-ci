c05fc51678896369783c3fa3a488bbf72eeeb8a9
Evgeny Slutsky <eslutsky@redhat.com>
2019-12-05 08:38:00 +0200

    ocp-on-rhv apply fixes

--- openshift/setup.sh
+++ openshift/setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+cd gcp-ws/
 git clone https://github.com/eslutsky/ocp-on-rhv-ci.git
 cd ocp-on-rhv-ci
 ln -s /runner/gcp-secrets/ocp-on-rhv-service.json ocp-on-rhv-service.json
+
+cat <<__EOF__ >~/.ansible.cfg
+[defaults]
+host_key_checking = False
+__EOF__
+
+
 ansible-inventory -i inventory.compute.gcp.yml host --list
-ansible-playbook -u centos -i inventory.compute.gcp.yml  rhv-on-gcp-scale.yml
+ansible-playbook -u centos --private-key=/runner/gcp-secrets/id_rsa -i inventory.compute.gcp.yml "$@"
+
+
 
 echo "cleaning up"
 cd ../
-rm -rf ocp-on-rhv-ci/
+#rm -rf ocp-on-rhv-ci/
